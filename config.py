2e59112ae416ab7f68ea9e936d59e902ef47b257
max-maslyuk <maxmas@list.ru>
2021-06-24 16:44:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'ODU3NjA2MzQ5NzM0MTUwMTY1.YNSCJw.5nK99r2YkeOCJsFtAfB8pQYVzkw',
+    'token': 'token bot place here!!!',
     'bot': 'CityGame',
     'client_id': '857606349734150165'
 }
